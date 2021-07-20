dcf751a7cd4f40d0cf2215cccc5c16aebee5e1ee
Iandi Santulus <iandisantulusn@gmail.com>
2021-07-21 05:07:34 +0700

    added export screens

---
+++ lib/screens.dart
@@ -0,0 +1,6 @@
+export 'views/splash_screen.dart';
+export 'views/favmovies/screen/_favmovies_screen.dart';
+export 'views/home/screen/_home_screen.dart';
+export 'views/home/tabs/nowplaying/screen/_nowplaying_screen.dart';
+export 'views/home/tabs/upcoming/screen/_upcoming_screen.dart';
+export 'views/moviedetail/screen/_moviedetail_screen.dart';
