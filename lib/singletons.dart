a0b09f09c9246e0846ac6847c21fb2b2494252b7
Iandi Santulus <iandisantulusn@gmail.com>
2021-07-21 05:05:29 +0700

    modified singletons.dart to pass abstractions of services and repositories

--- lib/singletons.dart
+++ lib/singletons.dart
@@ -1,13 +1,35 @@
-
 import 'package:get_it/get_it.dart';
-import 'package:http/http.dart' as http show Client;
 
+import 'core/repositories/favmovies_repo/base_favmovies_repo.dart';
+import 'core/repositories/favmovies_repo/favmovies_repo.dart';
+
+import 'core/repositories/movies_repo/base_movies_repo.dart';
+import 'core/repositories/movies_repo/movies_repo.dart';
+
+import 'core/services/localdb_service/base_localdb_service.dart';
 import 'core/services/localdb_service/localdb_service.dart';
-import '/core/services/navigation_service/navigation_service.dart';
+
+import 'core/services/navigation_service/base_navigation_service.dart';
+import 'core/services/navigation_service/navigation_service.dart';
+
+import 'core/services/network_service/base_network_service.dart';
 import 'core/services/network_service/network_service.dart';
 
-void initSingletons(){
-  GetIt.I.registerSingleton(NavigationService());
-  GetIt.I.registerSingleton(NetworkService(http.Client()));
-  GetIt.I.registerSingleton(LocalDbService());
+void initSingletons() {
+
+  // initialize services
+  GetIt.I.registerSingleton<BaseNetworkService>(NetworkService());
+  GetIt.I.registerSingleton<BaseLocalDbService>(LocalDbService()..initDb());
+  GetIt.I.registerLazySingleton<BaseNavigationService>(() => NavigationService());
+  
+  // initialize repositories
+  GetIt.I.registerSingleton<BaseMoviesRepository>(MoviesRepository(
+    networkService: GetIt.I<BaseNetworkService>(),
+  ));
+  GetIt.I.registerLazySingleton<BaseFavMoviesRepository>(
+    () => FavMoviesRepository(
+      localDbService: GetIt.I<BaseLocalDbService>(),
+    ),
+  );
+  
 }
