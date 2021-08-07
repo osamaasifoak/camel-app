eebe9cfdc54cabecde98b2a9155f788c6bc233b5
Iandi Santulus <iandisantulusn@gmail.com>
2021-08-07 19:01:02 +0700

    modified MovieDetail model

--- lib/core/models/movie/movie_detail.dart
+++ lib/core/models/movie/movie_detail.dart
@@ -1,6 +1,6 @@
 import 'dart:convert' show json;
 
-import 'package:camelmovies/core/constants/app_apis.dart';
+import '/core/constants/app_apis.dart';
 
 import 'movie.dart';
 import 'movie_genre.dart';
@@ -8,35 +8,36 @@ import 'movie_genre.dart';
 class MovieDetail extends Movie {
   final String? overview; //overview
   final String? imgUrlBackdrop; //backdrop_path
-  final num? runtime;
-  final String? year;
+  final int? runtime;
   final List<MovieGenre>? genres;
 
   const MovieDetail({
-    num? id,
+    required int id,
     String? title,
     String? releaseDate,
-    num? rating,
-    num? voteCount,
+    double? rating,
+    int? voteCount,
     String? imgUrlPoster,
+    String? year,
     this.overview,
     this.imgUrlBackdrop,
     this.runtime,
-    this.year,
     this.genres,
-  }): super(
-    id: id, 
-    title: title, 
-    releaseDate: releaseDate, 
-    rating: rating,
-    voteCount: voteCount,
-    imgUrlPoster: imgUrlPoster);
+  }) : super(
+          id: id,
+          title: title,
+          releaseDate: releaseDate,
+          rating: rating,
+          voteCount: voteCount,
+          imgUrlPoster: imgUrlPoster,
+          year: year,
+        );
 
   String get imgUrlBackdropOriginal {
     if (imgUrlBackdrop != null) {
       return AppApis().baseImageUrl + AppApis().epOriginalImage + imgUrlBackdrop!;
     } else {
-      return AppApis().baseImageUrl + AppApis().epOriginalImage + '/null';
+      return '${AppApis().baseImageUrl}${AppApis().epOriginalImage}/null';
     }
   }
 
@@ -44,21 +45,21 @@ class MovieDetail extends Movie {
     if (imgUrlBackdrop != null) {
       return AppApis().baseImageUrl + AppApis().epThumbImage + imgUrlBackdrop!;
     } else {
-      return AppApis().baseImageUrl + AppApis().epThumbImage + '/null';
+      return '${AppApis().baseImageUrl}${AppApis().epThumbImage}/null';
     }
   }
 
   @override
   MovieDetail copyWith({
-    num? id,
+    int? id,
     String? title,
     String? releaseDate,
     String? overview,
-    num? rating,
-    num? voteCount,
+    double? rating,
+    int? voteCount,
     String? imgUrlPoster,
     String? imgUrlBackdrop,
-    num? runtime,
+    int? runtime,
     String? year,
     List<MovieGenre>? genres,
   }) {
@@ -80,37 +81,32 @@ class MovieDetail extends Movie {
   @override
   Map<String, dynamic> toMap() {
     return {
-      'id': id,
-      'title': title,
-      'release_date': releaseDate,
+      ...super.toMap(),
       'overview': overview,
-      'vote_average': rating,
-      'vote_count': voteCount,
-      'poster_path': imgUrlPoster,
       'backdrop_path': imgUrlBackdrop,
       'runtime': runtime,
-      'year': year,
       'genres': genres,
     };
   }
 
   @override
   factory MovieDetail.fromMap(Map<String, dynamic> map) {
-    var gen = List<Map<String, dynamic>>.from(map['genres']);
-    var genres = List.generate(gen.length, (i) => 
-      MovieGenre.fromMap(gen[i])
-    );
+    final genres = (map['genres'] as List)
+        .map(
+          (g) => MovieGenre.fromMap(g as Map<String, dynamic>),
+        )
+        .toList(growable: false);
     return MovieDetail(
-      id: map['id'],
-      title: map['title'],
-      releaseDate: map['release_date'],
-      overview: map['overview'].toString(),
-      rating: map['vote_average'],
-      voteCount: map['vote_count'],
-      imgUrlPoster: map['poster_path'],
-      imgUrlBackdrop: map['backdrop_path'],
-      runtime: map['runtime'],
-      year: DateTime.parse(map['release_date']).year.toString(),
+      id: map['id'] as int,
+      title: map['title'] as String,
+      releaseDate: map['release_date'] as String,
+      overview: map['overview'] as String,
+      rating: map['vote_average'] as double,
+      voteCount: map['vote_count'] as int,
+      imgUrlPoster: map['poster_path'] as String?,
+      imgUrlBackdrop: map['backdrop_path'] as String?,
+      runtime: map['runtime'] as int,
+      year: DateTime.parse(map['release_date'] as String).year.toString(),
       genres: genres,
     );
   }
@@ -119,7 +115,7 @@ class MovieDetail extends Movie {
   String toJson() => json.encode(toMap());
 
   @override
-  factory MovieDetail.fromJson(String source) => MovieDetail.fromMap(json.decode(source));
+  factory MovieDetail.fromJson(String source) => MovieDetail.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
   String toString() {
@@ -129,33 +125,33 @@ class MovieDetail extends Movie {
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-  
+
     return other is MovieDetail &&
-      other.id == id &&
-      other.title == title &&
-      other.releaseDate == releaseDate &&
-      other.overview == overview &&
-      other.rating == rating &&
-      other.voteCount == voteCount &&
-      other.imgUrlPoster == imgUrlPoster &&
-      other.imgUrlBackdrop == imgUrlBackdrop &&
-      other.runtime == runtime &&
-      other.year == year &&
-      other.genres == genres;
+        other.id == id &&
+        other.title == title &&
+        other.releaseDate == releaseDate &&
+        other.overview == overview &&
+        other.rating == rating &&
+        other.voteCount == voteCount &&
+        other.imgUrlPoster == imgUrlPoster &&
+        other.imgUrlBackdrop == imgUrlBackdrop &&
+        other.runtime == runtime &&
+        other.year == year &&
+        other.genres == genres;
   }
 
   @override
   int get hashCode {
     return id.hashCode ^
-      title.hashCode ^
-      releaseDate.hashCode ^
-      overview.hashCode ^
-      rating.hashCode ^
-      voteCount.hashCode ^
-      imgUrlPoster.hashCode ^
-      imgUrlBackdrop.hashCode ^
-      runtime.hashCode ^
-      year.hashCode ^
-      genres.hashCode;
+        title.hashCode ^
+        releaseDate.hashCode ^
+        overview.hashCode ^
+        rating.hashCode ^
+        voteCount.hashCode ^
+        imgUrlPoster.hashCode ^
+        imgUrlBackdrop.hashCode ^
+        runtime.hashCode ^
+        year.hashCode ^
+        genres.hashCode;
   }
 }
