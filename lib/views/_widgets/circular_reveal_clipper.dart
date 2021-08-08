baebc87bf1f3fa22844af8fe04c47a9da8ebd9f1
Iandi Santulus <iandisantulusn@gmail.com>
2021-08-08 10:23:33 +0700

    moved CircularRevealClipper

---
+++ lib/views/_widgets/circular_reveal_clipper.dart
@@ -0,0 +1,53 @@
+import 'dart:math' show max, sqrt ;
+
+import 'package:flutter/material.dart';
+
+/// Copyright 2021 Alexander Zhdanov
+/// https://github.com/qwert2603
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+@immutable
+class CircularRevealClipper extends CustomClipper<Path> {
+  
+  const CircularRevealClipper({
+    required this.fraction,
+    this.centerAlignment,
+    this.centerOffset,
+    this.minRadius,
+    this.maxRadius,
+  });
+
+  final double fraction;
+  final Alignment? centerAlignment;
+  final Offset? centerOffset;
+  final double? minRadius;
+  final double? maxRadius;
+
+  @override
+  Path getClip(Size size) {
+    final Offset center = centerAlignment?.alongSize(size) ?? centerOffset ?? Offset(size.width / 2, size.height / 2);
+    final minRadius = this.minRadius ?? 0;
+    final maxRadius = this.maxRadius ?? calcMaxRadius(size, center);
+
+    return Path()
+      ..addOval(
+        Rect.fromCircle(
+          center: center,
+          radius: lerpDouble(minRadius, maxRadius, fraction),
+        ),
+      );
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+
+  static double calcMaxRadius(Size size, Offset center) {
+    final w = max(center.dx, size.width - center.dx);
+    final h = max(center.dy, size.height - center.dy);
+    return sqrt(w * w + h * h);
+  }
+
+  static double lerpDouble(double a, double b, double t) {
+    return a * (1.0 - t) + b * t;
+  }
+}
