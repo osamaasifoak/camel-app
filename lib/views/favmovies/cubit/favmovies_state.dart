214f503d562d2a80fb194968c02896f0fbfd8c7a
Iandi Santulus <iandisantulusn@gmail.com>
2021-08-08 10:49:42 +0700

    modified FavMoviesState revamped the whole FavMoviesState

--- lib/views/favmovies/cubit/favmovies_state.dart
+++ lib/views/favmovies/cubit/favmovies_state.dart
@@ -1,37 +1,85 @@
 part of 'favmovies_cubit.dart';
 
-abstract class FavMoviesState extends Equatable {
-  
-  const FavMoviesState();
-
-  @override
-  List<Object?> get props => const [];
-
+enum FavMoviesStatus {
+  init,
+  loading,
+  loaded,
+  loadingMore,
+  error,
 }
 
-class FavMoviesLoading extends FavMoviesState {
-  const FavMoviesLoading();
-}
-
-class FavMoviesError extends FavMoviesState {
-  final String errorMessage;
-  const FavMoviesError(this.errorMessage);
-
-  @override
-  List<Object?> get props => [errorMessage];
-}
-
-class FavMoviesLoaded extends FavMoviesState {
-
-  final List<Movie> movies;
-  // final int favCount;
-
-  const FavMoviesLoaded({
-    required this.movies, 
-    // required this.favCount,
+class FavMoviesState extends Equatable {
+  const FavMoviesState({
+    required this.status,
+    required this.currentPage,
+    required this.movies,
+    this.isAtEndOfPage = false,
+    this.errorMessage,
   });
 
-  @override
-  List<Object> get props => [movies, /* favCount */];
+  final FavMoviesStatus status;
+  final int currentPage;
+  final List<Movie> movies;
+  final bool isAtEndOfPage;
+  final String? errorMessage;
 
+  bool get isLoading => status == FavMoviesStatus.loading;
+  bool get isLoadingMore => status == FavMoviesStatus.loadingMore;
+  bool get isBusy => isLoading || isLoadingMore;
+
+  bool get hasError => status == FavMoviesStatus.error;
+
+  factory FavMoviesState.init() {
+    return const FavMoviesState(
+      status: FavMoviesStatus.init,
+      currentPage: 0,
+      movies: [],
+    );
+  }
+
+  FavMoviesState loading() {
+    return const FavMoviesState(
+      status: FavMoviesStatus.loading,
+      currentPage: 0,
+      movies: [],
+    );
+  }
+
+  FavMoviesState loadingMore({
+    List<Movie>? movies,
+  }) {
+    return FavMoviesState(
+      status: FavMoviesStatus.loadingMore,
+      currentPage: currentPage,
+      movies: movies ?? this.movies,
+    );
+  }
+
+  FavMoviesState loaded({
+    required List<Movie> movies,
+    bool? isAtEndOfPage,
+    int? newPage,
+  }) {
+    return FavMoviesState(
+      status: FavMoviesStatus.loaded,
+      isAtEndOfPage: isAtEndOfPage ?? this.isAtEndOfPage,
+      currentPage: newPage ?? currentPage,
+      movies: movies,
+    );
+  }
+
+  FavMoviesState error({
+    required String errorMessage,
+  }) {
+    return FavMoviesState(
+      status: FavMoviesStatus.error,
+      isAtEndOfPage: isAtEndOfPage,
+      currentPage: currentPage,
+      movies: movies,
+      errorMessage: errorMessage,
+    );
+  }
+
+  @override
+  List<Object?> get props => [status, currentPage, movies, isAtEndOfPage];
 }
