ba3d40162b22cb1028770abb1e698d9212ecf599
Iandi Santulus <iandisantulusn@gmail.com>
2021-08-08 10:27:54 +0700

    created PageNotFoundScreen

---
+++ lib/views/page_not_found_screen.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+
+import '_widgets/error_screen.dart';
+
+class PageNotFoundScreen extends StatelessWidget {
+  const PageNotFoundScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: Navigator.of(context).pop,
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+          ),
+        ),
+      ),
+      body: Center(
+        child: ErrorScreen(
+          errorMessage: "Oopss... The page you're looking for does not exist",
+          onRetry: Navigator.of(context).pop,
+        ),
+      ),
+    );
+  }
+}
