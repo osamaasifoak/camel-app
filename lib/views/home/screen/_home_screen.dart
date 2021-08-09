242da2f566874af275295ddd6e0f6d8cefce8f4f
Iandi Santulus <iandisantulusn@gmail.com>
2021-08-10 02:50:24 +0700

    modified HomeScreen
    # added animation on appbar when changing PageView to profile screen
    # added animation on fav icon when changing PageView

--- lib/views/home/screen/_home_screen.dart
+++ lib/views/home/screen/_home_screen.dart
@@ -1,16 +1,14 @@
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 import '/core/constants/app_routes.dart';
+import '/core/repositories/fav_tv_shows_repo/base_fav_tv_shows_repo.dart';
 import '/core/repositories/favmovies_repo/base_favmovies_repo.dart';
 import '/core/services/localdb_service/base_localdb_service.dart';
 import '/core/services/network_service/base_network_service.dart';
-import '/views/home/tabs/nowplaying/cubit/nowplaying_cubit.dart';
-import '/views/home/tabs/nowplaying/screen/_nowplaying_screen.dart';
-import '/views/home/tabs/upcoming/cubit/upcoming_cubit.dart';
-import '/views/home/tabs/upcoming/screen/_upcoming_screen.dart';
+import '/views/home/tabs/movies_sections/screen/_movies_sections_screen.dart';
+import '/views/home/tabs/profile/profile_screen.dart';
+import '/views/home/tabs/tv_shows_sections/screen/_tvshows_sections_screen.dart';
 
 part 'home_screen_props.dart';
 part 'home_screen_widgets.dart';
@@ -27,11 +25,54 @@ class _HomeScreenState extends _HomeScreenProps with _HomeScreenWidgets {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'caMel',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF01579B),
+        title: ValueListenableBuilder<int>(
+          valueListenable: _bottomNavSelectedIndex,
+          builder: (_, index, child) {
+            return AnimatedSwitcher(
+              duration: const Duration(milliseconds: 500),
+              transitionBuilder: (child, animation) {
+                return AnimatedBuilder(
+                  animation: animation,
+                  builder: (_, child) {
+                    return Opacity(
+                      opacity: animation.value,
+                      child: Transform.translate(
+                        offset: Offset(0, (1.0 - animation.value) * 20),
+                        child: child,
+                      ),
+                    );
+                  },
+                  child: child,
+                );
+              },
+              layoutBuilder: (currentChild, previousChildren) {
+                return Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    ...previousChildren,
+                    if (currentChild != null) currentChild,
+                  ],
+                );
+              },
+              child: index != 2
+                  ? child!
+                  : const Text(
+                      'about Me',
+                      key: ValueKey('about-dev'),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF01579B),
+                      ),
+                    ),
+            );
+          },
+          child: const Text(
+            'caMel',
+            key: ValueKey('app-name'),
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF01579B),
+            ),
           ),
         ),
         actions: [
@@ -43,17 +84,21 @@ class _HomeScreenState extends _HomeScreenProps with _HomeScreenWidgets {
       ),
       body: PageView(
         controller: _pageController,
-        onPageChanged: (index) => _selectedIndex.value = index,
-        children: moviePages,
+        onPageChanged: _onPageChanged,
+        allowImplicitScrolling: true,
+        children: bottomNavPages,
       ),
       bottomNavigationBar: ValueListenableBuilder<int>(
-        valueListenable: _selectedIndex,
+        valueListenable: _bottomNavSelectedIndex,
         builder: (_, index, __) => BottomNavigationBar(
-          items: bottomNavItems,
+          type: BottomNavigationBarType.shifting,
           currentIndex: index,
           onTap: _onBottomNavTapped,
           selectedItemColor: const Color(0xFF01579B),
+          unselectedItemColor: Colors.grey,
           selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+          showUnselectedLabels: true,
+          items: bottomNavItems,
         ),
       ),
     );
