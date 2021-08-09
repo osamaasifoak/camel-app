6c36a4f690164656ec9d663b46947e67bfc0f008
Iandi Santulus <iandisantulusn@gmail.com>
2021-08-10 02:19:39 +0700

    modified AppRoutes added new routes introduced new way of routing to detail screen

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -1,7 +1,33 @@
-
 class AppRoutes {
   static const String home = '/';
   static const String splash = '/splash';
-  static const String movieDetail = '/detail';
-  static const String favMovies = '/favs';
+
+  static const String movieDetail = '/movie';
+  static const String nowPlayingMovieList = '/movies/now_playing';
+  static const String popularMovieList = '/movies/popular';
+  static const String upcomingMovieList = '/movies/upcoming';
+  static const String favMovies = '/fav/movies';
+
+  static const String tvShowDetail = '/tv';
+  static const String popularTVShowList = '/tv_shows/popular';
+  static const String onTheAirTVShowList = '/tv_shows/on_the_air';
+  static const String favTVShows = '/fav/tv_shows';
+
+  static String getMovieDetail(int movieId) {
+    return Uri(
+      path: movieDetail,
+      queryParameters: {
+        'id': movieId.toString(),
+      },
+    ).toString();
+  }
+
+  static String getTVShowDetail(int tvShowId) {
+    return Uri(
+      path: tvShowDetail,
+      queryParameters: {
+        'id': tvShowId.toString(),
+      },
+    ).toString();
+  }
 }
