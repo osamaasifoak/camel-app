1db0f0f1654129eec02f8e223783fd4ceb4883c2
Iandi Santulus <iandisantulusn@gmail.com>
2021-08-10 02:51:12 +0700

    modified HomeScreenProps
    # removed unused methods

--- lib/views/home/screen/home_screen_props.dart
+++ lib/views/home/screen/home_screen_props.dart
@@ -1,27 +1,23 @@
 part of '_home_screen.dart';
 
 abstract class _HomeScreenProps extends State<HomeScreen> with SingleTickerProviderStateMixin {
-  
-  final _selectedIndex = ValueNotifier(0);
-  
-  final _nowPlayingScrollController = ScrollController();
-  final _upcomingScrollController = ScrollController();
-  
+  final _bottomNavSelectedIndex = ValueNotifier(0);
+
   final _favMoviesRepo = GetIt.I<BaseFavMoviesRepository>();
+  final _favTVShowsRepo = GetIt.I<BaseFavTVShowsRepository>();
 
   final _pageController = PageController();
 
   @override
   void initState() {
     super.initState();
-    _favMoviesRepo.getFavCount().then(_favMoviesRepo.favCountController.add);
+    _favMoviesRepo.refreshFavMoviesCount();
+    _favTVShowsRepo.refreshFavTVShowsCount();
   }
 
   @override
-  void dispose() { 
-    _selectedIndex.dispose();
-    _nowPlayingScrollController.dispose();
-    _upcomingScrollController.dispose();
+  void dispose() {
+    _bottomNavSelectedIndex.dispose();
     _pageController.dispose();
     _favMoviesRepo.close();
 
@@ -31,49 +27,26 @@ abstract class _HomeScreenProps extends State<HomeScreen> with SingleTickerProvi
     super.dispose();
   }
 
-  void _loadFavMovies() {
-    Navigator.of(context).pushNamed(AppRoutes.favMovies);
+  // ignore: use_setters_to_change_properties
+  void _onPageChanged(int index) {
+    _bottomNavSelectedIndex.value = index;
   }
 
-  void _scrollNowPlaying() {
-    _nowPlayingScrollController.animateTo(
-      0.0, 
-      duration: const Duration(milliseconds: 500), 
-      curve: Curves.fastLinearToSlowEaseIn,
-    );
-  }
-
-  void _scrollUpcoming() {
-    _upcomingScrollController.animateTo(
-      0,
-      duration: const Duration(milliseconds: 500),
-      curve: Curves.fastLinearToSlowEaseIn,
-    );
+  void _loadFavorites() {
+    if(_bottomNavSelectedIndex.value == 0) {
+      Navigator.of(context).pushNamed(AppRoutes.favMovies);
+    } else if(_bottomNavSelectedIndex.value == 1) {
+      Navigator.of(context).pushNamed(AppRoutes.favTVShows);
+    }
   }
 
   void _onBottomNavTapped(int index) {
-
-    if(_selectedIndex.value == index) {
-
-      if(index == 0 && _nowPlayingScrollController.offset > 0.0) {
-        
-        _scrollNowPlaying();
-
-      }else if(index == 1 && _upcomingScrollController.offset > 0.0) {
-
-        _scrollUpcoming();
-
-      }
-
-      return;
+    if (_bottomNavSelectedIndex.value != index) {
+      _pageController.animateToPage(
+        index,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.fastLinearToSlowEaseIn,
+      );
     }
-
-    _selectedIndex.value = index;
-    _pageController.animateToPage(
-      index,
-      duration: const Duration(milliseconds: 500),
-      curve: Curves.fastLinearToSlowEaseIn,
-    );
   }
-
 }
