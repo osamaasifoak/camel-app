448cc7ca37c90054b3ef56bdf7f2dcf1d63c57c1
Iandi Santulus <iandisantulusn@gmail.com>
2021-08-25 22:04:04 +0700

    modified singletons
    * deleted usage of NetworkService
    * deleted usage of Movies2Repo

--- lib/singletons.dart
+++ lib/singletons.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:postor/postor.dart';
 
 import '/core/repositories/fav_tv_shows_repo/base_fav_tv_shows_repo.dart';
 import '/core/repositories/fav_tv_shows_repo/fav_tv_shows_repo.dart';
@@ -6,31 +7,34 @@ import '/core/repositories/fav_tv_shows_repo/fav_tv_shows_repo.dart';
 import '/core/repositories/tv_show_repo/base_tv_show_repo.dart';
 import '/core/repositories/tv_show_repo/tv_show_repo.dart';
 
+import 'core/constants/app_apis.dart';
 import 'core/repositories/favmovies_repo/base_favmovies_repo.dart';
 import 'core/repositories/favmovies_repo/favmovies_repo.dart';
 
 import 'core/repositories/movies_repo/base_movies_repo.dart';
-import 'core/repositories/movies_repo/movies2_repo/movies2_repo.dart';
+import 'core/repositories/movies_repo/movies_repo.dart';
 
 import 'core/services/localdb_service/base_localdb_service.dart';
 import 'core/services/localdb_service/localdb_service.dart';
-
-import 'core/services/network_service/base_network_service.dart';
-import 'core/services/network_service/network_service.dart';
 
 import 'core/services/screen_messenger/base_screen_messenger.dart';
 import 'core/services/screen_messenger/screen_messenger.dart';
 
 void initSingletons() {
   // initialize services
-  GetIt.I.registerSingleton<BaseNetworkService>(NetworkService());
+  GetIt.I.registerSingleton<Postor>(
+    Postor(
+      AppApis().baseUrl,
+      defaultHeaders: AppApis().defaultHeader,
+    ),
+  );
   GetIt.I.registerSingleton<BaseLocalDbService>(LocalDbService());
   GetIt.I.registerLazySingleton<BaseScreenMessenger>(() => ScreenMessenger());
 
   // initialize repositories
-  GetIt.I.registerSingleton<BaseMoviesRepository>(Movies2Repository());
+  GetIt.I.registerSingleton<BaseMoviesRepository>(MoviesRepository());
   GetIt.I.registerSingleton<BaseTVShowRepository>(TVShowRepository());
-  
+
   GetIt.I.registerSingleton<BaseFavMoviesRepository>(FavMoviesRepository());
   GetIt.I.registerSingleton<BaseFavTVShowsRepository>(FavTVShowsRepository());
 }
