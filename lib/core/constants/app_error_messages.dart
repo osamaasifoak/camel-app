c89ece6c5c17a9370e04d94459e782e4aab9e6ba
Iandi Santulus <iandisantulusn@gmail.com>
2021-08-29 19:31:05 +0700

    modified AppErrorMessages
    added a few error messages for movies & tv shows errors

--- lib/core/constants/app_error_messages.dart
+++ lib/core/constants/app_error_messages.dart
@@ -7,4 +7,13 @@ class AppErrorMessages {
   static const String unknownError = 'An unknown error occurred. Please try again.';
   static const String unknownRequestError = 'An unknown error occurred while requesting to server. Please try again.';
   static const String cancelledRequestError = 'Network request was cancelled.';
+
+  static const String nowPlayingMoviesUnknownError = 'Failed to fetch now playing movies. Please try again.';
+  static const String popularMoviesUnknownError = 'Failed to fetch popular movies. Please try again.';
+  static const String upcomingMoviesUnknownError = 'Failed to fetch upcoming movies. Please try again.';
+  static const String favMoviesUnknownError = 'Failed to fetch favourite movies. Please try again.';
+
+  static const String onTheAirTVShowsUnknownError = 'Failed to fetch on the air TV shows. Please try again.';
+  static const String popularTVShowsUnknownError = 'Failed to fetch popular TV shows. Please try again.';
+  static const String favTVShowsUnknownError = 'Failed to fetch favourite TV shows. Please try again.';
 }
