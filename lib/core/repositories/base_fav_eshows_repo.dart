2cad07cbbbed6be5d7f4846698bc1cde299d4e93
Iandi Santulus <iandisantulusn@gmail.com>
2021-08-31 05:03:49 +0700

    added BaseFavEShowsRepository
    this is intended to reduce duplication of favorited shows repo

---
+++ lib/core/repositories/base_fav_eshows_repo.dart
@@ -0,0 +1,19 @@
+import 'package:rxdart/rxdart.dart' show BehaviorSubject;
+import '/core/models/fav_entertainment_show/fav_entertainment_show.dart';
+
+abstract class BaseFavEShowsRepository {
+  BehaviorSubject<int> get favCountController;
+  void refreshFavCount();
+  Future<int> getFavCount();
+  /// [page] starts from 0
+  /// 
+  /// [perPage] max items to be returned
+  Future<List<int>> getFavList({
+    int page = 0,
+    int perPage = 10,
+  });
+  Future<void> insertFav(FavEShow favEShow);
+  Future<void> deleteFav(int id);
+  Future<bool> isFav(int id);
+  Future<void> close();
+}
