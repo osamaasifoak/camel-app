15c94674f8fea6d38a208012f85230c4e25f08b4
Iandi Santulus <iandisantulusn@gmail.com>
2021-08-30 13:00:36 +0700

    modified main.dart
    - set postor's defaultJsonDecoder to using compute function
    - added listener for pointer down event; this is intended for creating transition animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:convert' show jsonDecode;
+
+import 'package:flutter/foundation.dart' as foundation show compute;
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/services.dart' show SystemChrome, SystemUiOverlayStyle;
+import 'package:postor/postor.dart' as postor show defaultJsonDecoder;
 
 import 'core/constants/app_apis.dart';
 import 'core/constants/app_routes.dart';
@@ -19,9 +23,15 @@ Future<void> main() async {
 
   initSingletons();
 
+  postor.defaultJsonDecoder = appDefaultJsonDecoder;
+
   initErrorMessageHandlers();
 
   runApp(const App());
+}
+
+Future<dynamic> appDefaultJsonDecoder(String source) {
+  return foundation.compute(jsonDecode, source);
 }
 
 class App extends StatelessWidget {
@@ -29,18 +39,21 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'caMel',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.white,
-        accentColor: const Color(0xFF01579B),
-        errorColor: const Color(0xFFD50000),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        fontFamily: 'Nunito Sans',
+    return Listener(
+      onPointerDown: ScreenRouter().onPointerDownEvent,
+      child: MaterialApp(
+        title: 'caMel',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.white,
+          accentColor: const Color(0xFF01579B),
+          errorColor: const Color(0xFFD50000),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: 'Nunito Sans',
+        ),
+        initialRoute: AppRoutes.splash,
+        onGenerateRoute: ScreenRouter().onGenerateRoute,
       ),
-      initialRoute: AppRoutes.splash,
-      onGenerateRoute: ScreenRouter.onGenerateRoute,
     );
   }
 }
