840921cc8a8b75f1208ca38cc278fe5bfe91d9ce
Iandi Santulus <iandisantulusn@gmail.com>
2021-08-31 05:06:37 +0700

    modified singletons
    changed type for initializing both FavMoviesRepo & FavTVShowsRepo

--- lib/singletons.dart
+++ lib/singletons.dart
@@ -1,18 +1,16 @@
 import 'package:get_it/get_it.dart';
 import 'package:postor/postor.dart';
 
-import '/core/repositories/fav_tv_shows_repo/base_fav_tv_shows_repo.dart';
-import '/core/repositories/fav_tv_shows_repo/fav_tv_shows_repo.dart';
-
-import '/core/repositories/tv_show_repo/base_tv_show_repo.dart';
-import '/core/repositories/tv_show_repo/tv_show_repo.dart';
-
 import 'core/constants/app_apis.dart';
-import 'core/repositories/favmovies_repo/base_favmovies_repo.dart';
+import 'core/repositories/base_fav_eshows_repo.dart';
+import 'core/repositories/fav_tv_shows_repo/fav_tv_shows_repo.dart';
 import 'core/repositories/favmovies_repo/favmovies_repo.dart';
 
 import 'core/repositories/movies_repo/base_movies_repo.dart';
 import 'core/repositories/movies_repo/movies_repo.dart';
+
+import 'core/repositories/tv_show_repo/base_tv_show_repo.dart';
+import 'core/repositories/tv_show_repo/tv_show_repo.dart';
 
 import 'core/services/localdb_service/base_localdb_service.dart';
 import 'core/services/localdb_service/localdb_service.dart';
@@ -46,6 +44,12 @@ void initSingletons() {
   GetIt.I.registerSingleton<BaseMoviesRepository>(MoviesRepository());
   GetIt.I.registerSingleton<BaseTVShowRepository>(TVShowRepository());
 
-  GetIt.I.registerSingleton<BaseFavMoviesRepository>(FavMoviesRepository());
-  GetIt.I.registerSingleton<BaseFavTVShowsRepository>(FavTVShowsRepository());
+  GetIt.I.registerSingleton<BaseFavEShowsRepository>(
+    FavMoviesRepository(),
+    instanceName: 'fav-movies',
+  );
+  GetIt.I.registerSingleton<BaseFavEShowsRepository>(
+    FavTVShowsRepository(),
+    instanceName: 'fav-tvshows',
+  );
 }
