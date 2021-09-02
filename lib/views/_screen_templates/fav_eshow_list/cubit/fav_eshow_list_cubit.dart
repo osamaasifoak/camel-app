c7983d7a4793ddf6e9a564459851fb604fc1a7a1
Iandi Santulus <iandisantulusn@gmail.com>
2021-09-02 23:06:09 +0700

    modified FavEShowListCubit
    it now requires BaseEShowsRepository and BaseFavEShowsRepository instead of callbacks

--- lib/views/_screen_templates/fav_eshow_list/cubit/fav_eshow_list_cubit.dart
+++ lib/views/_screen_templates/fav_eshow_list/cubit/fav_eshow_list_cubit.dart
@@ -3,25 +3,24 @@ import 'package:postor/error_handler.dart' as eh show catchIt;
 
 import '/core/enums/state_status.dart';
 import '/core/models/entertainment_show/entertainment_show.dart';
+import '/core/repositories/base_eshows_repo.dart';
+import '/core/repositories/base_fav_eshows_repo.dart';
 import '/views/_screen_templates/base_bloc_state.dart';
 
 part 'fav_eshow_list_state.dart';
 
-typedef LoadFavEShowIDsListCallback = Future<List<int>> Function({int page, int perPage});
-typedef LoadEShowListByIDsCallback = Future<List<EShow>> Function(List<int> eShowIds);
-
 class FavEShowListCubit extends Cubit<FavEShowListState> {
   FavEShowListCubit({
-    required LoadFavEShowIDsListCallback loadFavEShowIDsListCallback,
-    required LoadEShowListByIDsCallback loadEShowListByIDsCallback,
+    required BaseEShowsRepository eShowsRepo,
+    required BaseFavEShowsRepository favEShowRepo,
     required String unknownErrorMessage,
-  })  : _loadFavEShowIDsListCallback = loadFavEShowIDsListCallback,
-        _loadEShowListByIDsCallback = loadEShowListByIDsCallback,
+  })  : _eShowsRepo = eShowsRepo,
+        _favEShowRepo = favEShowRepo,
         _unknownErrorMessage = unknownErrorMessage,
         super(FavEShowListState.init());
 
-  final LoadFavEShowIDsListCallback _loadFavEShowIDsListCallback;
-  final LoadEShowListByIDsCallback _loadEShowListByIDsCallback;
+  final BaseEShowsRepository _eShowsRepo;
+  final BaseFavEShowsRepository _favEShowRepo;
   final String _unknownErrorMessage;
 
   Future<void> loadFavEShows({bool more = false}) async {
@@ -35,7 +34,7 @@ class FavEShowListCubit extends Cubit<FavEShowListState> {
     try {
       final int nextPage = more ? state.currentPage + 1 : 0;
 
-      final List<int> localFavEShowIDs = await _loadFavEShowIDsListCallback(page: nextPage);
+      final List<int> localFavEShowIDs = await _favEShowRepo.getFavList(page: nextPage);
       if (localFavEShowIDs.isEmpty) {
         emit(state.update(
           status: StateStatus.loaded,
@@ -48,16 +47,16 @@ class FavEShowListCubit extends Cubit<FavEShowListState> {
         final int firstHalf = localFavEShowIDs.length ~/ 2;
         final int secondHalf = localFavEShowIDs.length;
 
-        final List<EShow> firstHalfList = await _loadEShowListByIDsCallback(
-          localFavEShowIDs.getRange(0, firstHalf).toList(growable: false),
+        final List<EShow> firstHalfList = await _eShowsRepo.fetchByIds(
+          ids: localFavEShowIDs.getRange(0, firstHalf).toList(growable: false),
         );
         emit(state.update(
           status: StateStatus.loadingMore,
           favEShows: more ? state.favEShows + firstHalfList : firstHalfList,
         ));
 
-        final List<EShow> secondHalfList = await _loadEShowListByIDsCallback(
-          localFavEShowIDs.getRange(firstHalf, secondHalf).toList(growable: false),
+        final List<EShow> secondHalfList = await _eShowsRepo.fetchByIds(
+          ids: localFavEShowIDs.getRange(firstHalf, secondHalf).toList(growable: false),
         );
         emit(state.update(
           status: StateStatus.loaded,
