12e0f3c0ba31bae38351aa8232c82660e61b532f
Iandi Santulus <iandisantulusn@gmail.com>
2021-09-03 03:08:46 +0700

    fixed SIName
    favMovies should not self referencing

--- lib/core/constants/singletons_names.dart
+++ lib/core/constants/singletons_names.dart
@@ -8,7 +8,7 @@ class SIName {
 class _SingletonRepositories {
   const _SingletonRepositories();
   String get movies => 'movies';
-  String get favMovies => SIName.repo.favMovies;
+  String get favMovies => 'fav-movies';
   String get tvShows => 'tv-shows';
   String get favTVShows => 'fav-tvshows';
 }
