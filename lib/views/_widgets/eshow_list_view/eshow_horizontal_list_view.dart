77c4987fb80aadabf57d8d8093b7e9c7fbdc5e16
Iandi Santulus <iandisantulusn@gmail.com>
2021-09-03 02:47:13 +0700

    added EShowHorizontalListView

---
+++ lib/views/_widgets/eshow_list_view/eshow_horizontal_list_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../eshow_card/eshow_card.dart';
+import '../eshow_card/eshow_card_loading_indicator.dart';
+import '/core/models/entertainment_show/entertainment_show.dart';
+
+class EShowHorizontalListView extends StatelessWidget {
+  const EShowHorizontalListView({
+    Key? key,
+    required this.eShows,
+    required this.onEShowTapped,
+    this.itemExtent = 200,
+  }) : super(key: key);
+
+  final List<EShow> eShows;
+  final EShowCardCallback onEShowTapped;
+  final double itemExtent;
+
+  @override
+  Widget build(BuildContext context) {
+    if (eShows.isEmpty) {
+      return const EShowCardLoadingIndicator();
+    }
+    return SizedBox(
+      height: 450,
+      child: ListView.builder(
+        physics: const PageScrollPhysics(),
+        padding: const EdgeInsets.all(8.0),
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int index) {
+          return EShowCard(
+            eShow: eShows[index],
+            onTap: onEShowTapped,
+          );
+        },
+        itemExtent: itemExtent,
+        itemCount: eShows.length,
+      ),
+    );
+  }
+}
