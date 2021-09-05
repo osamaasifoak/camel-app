8e0e9c4e61616924e6d4902e1f7286456287c4b0
Iandi Santulus <iandisantulusn@gmail.com>
2021-09-06 02:04:16 +0700

    clean up AppApis

--- lib/core/constants/app_apis.dart
+++ lib/core/constants/app_apis.dart
@@ -9,8 +9,6 @@ enum MovieEndpoint {
   nowPlaying,
   upcoming,
   popular,
-  @Deprecated('Use [AppApis().movieReviewsOf] instead')
-  reviews,
 }
 
 extension MovieEndpointName on MovieEndpoint {
@@ -26,8 +24,6 @@ extension MovieEndpointName on MovieEndpoint {
         return '/3/movie/upcoming';
       case 4:
         return '/3/movie/popular';
-      case 5:
-      // return '/3/movie/<movieId>/reviews';
       default:
         throw IndexError(
           index,
@@ -42,8 +38,6 @@ enum TVEndpoint {
   popular,
   details,
   search,
-  @Deprecated('Use [AppApis().tvShowReviewsOf] instead')
-  reviews,
 }
 
 extension TVEndpointName on TVEndpoint {
@@ -57,8 +51,6 @@ extension TVEndpointName on TVEndpoint {
         return '/3/tv/';
       case 3:
         return '/3/search/tv';
-      case 4:
-      // return '/3/tv/<tvId>/reviews';
       default:
         throw IndexError(
           index,
@@ -146,8 +138,8 @@ class AppApis {
   AppApis._internal();
 
   final String baseImageUrl = 'https://image.tmdb.org';
-  final String epThumbImage = '/t/p/w500';
-  final String epOriginalImage = '/t/p/original';
+  final String epThumbImage = '/t/p/w400';
+  final String epOriginalImage = '/t/p/w500';
 
   final String baseUrl = 'api.themoviedb.org';
 
@@ -198,166 +190,5 @@ class AppApis {
       if (searchKeyword != null) _paramSearchKeyword: searchKeyword,
     };
     return params;
-  }
-
-  /// [endpoint] get either [MovieEndpoint] or [TVEndpoint] endpoint
-  ///
-  /// [page] is used for:
-  /// * [MovieEndpoint.nowPlaying]
-  /// * [MovieEndpoint.upcoming]
-  /// * [MovieEndpoint.popular]
-  /// * [MovieEndpoint.search]
-  ///
-  /// [id] can be `movieId` or `tvId` depends on [endpoint]
-  /// and is used for [MovieEndpoint.details]
-  ///
-  /// [keyword] is used for [MovieEndpoint.search]
-  ///
-  /// [language] pass an ISO 639-1 value to display translated data for the fields that support it.
-  /// defaults to [kDefaultLanguage] `en-US`
-  ///
-  /// [region] specify a ISO 3166-1 code to filter release dates. Must be uppercase.
-  /// defaults to [kDefaultRegion] `US`
-  @Deprecated('We no longer use this since using [Postor]. Use [paramsOf] instead')
-  Uri endpointOf(
-    Object endpoint, {
-    int? page,
-    String? id,
-    String? keyword,
-    String language = kDefaultLanguage,
-    String region = kDefaultRegion,
-  }) {
-    if (endpoint is MovieEndpoint) {
-      return _movieEndpointOf(
-        endpoint,
-        page: page,
-        keyword: keyword,
-        movieId: id,
-        language: language,
-        region: region,
-      );
-    } else if (endpoint is TVEndpoint) {
-      return _tvEndpointOf(
-        endpoint,
-        page: page,
-        keyword: keyword,
-        tvId: id,
-        language: language,
-        region: region,
-      );
-    }
-
-    throw ArgumentError('You have to choose between Movie or TV endpoint');
-  }
-
-  @Deprecated('We no longer use this since using [Postor]. Use [paramsOf] instead')
-  Uri _movieEndpointOf(
-    MovieEndpoint endpoint, {
-    required String language,
-    required String region,
-    int? page,
-    String? keyword,
-    String? movieId,
-  }) {
-    final String endpointName;
-    final params = {
-      _paramApiKey: _cachedApiKey,
-      _paramLanguage: language,
-      _paramRegion: region,
-    };
-    switch (endpoint) {
-      case MovieEndpoint.nowPlaying:
-      case MovieEndpoint.upcoming:
-      case MovieEndpoint.popular:
-      case MovieEndpoint.search:
-        assert(
-          movieId == null,
-          "Don't set [movieId] if not needed",
-        );
-        params[_paramPage] = page!.toString();
-        if (endpoint == MovieEndpoint.search) {
-          params[_paramSearchKeyword] = keyword!;
-        }
-        endpointName = endpoint.name;
-        break;
-      case MovieEndpoint.details:
-        assert(
-          page == null && keyword == null,
-          "Don't set [page] and [keyword] if not needed",
-        );
-        endpointName = endpoint.name + movieId!;
-        break;
-      case MovieEndpoint.reviews:
-        assert(
-          keyword == null,
-          "Don't set [keyword] if not needed",
-        );
-        params[_paramPage] = page!.toString();
-        endpointName = endpoint.name.replaceFirst(
-          '<movieId>',
-          movieId!,
-        );
-        break;
-    }
-    return Uri.https(
-      baseUrl,
-      endpointName,
-      params,
-    );
-  }
-
-  @Deprecated('We no longer use this since using [Postor]. Use [paramsOf] instead')
-  Uri _tvEndpointOf(
-    TVEndpoint endpoint, {
-    required String language,
-    required String region,
-    int? page,
-    String? keyword,
-    String? tvId,
-  }) {
-    final String endpointName;
-    final params = {
-      _paramApiKey: _cachedApiKey,
-      _paramLanguage: language,
-      _paramRegion: region,
-    };
-    switch (endpoint) {
-      case TVEndpoint.popular:
-      case TVEndpoint.onTheAir:
-      case TVEndpoint.search:
-        assert(
-          tvId == null,
-          "Don't set [tvId] if not needed",
-        );
-        params[_paramPage] = page!.toString();
-        if (endpoint == TVEndpoint.search) {
-          params[_paramSearchKeyword] = keyword!;
-        }
-        endpointName = endpoint.name;
-        break;
-      case TVEndpoint.details:
-        assert(
-          page == null && keyword == null,
-          "Don't set [page] and [keyword] if not needed",
-        );
-        endpointName = endpoint.name + tvId!;
-        break;
-      case TVEndpoint.reviews:
-        assert(
-          keyword == null,
-          "Don't set [keyword] if not needed",
-        );
-        params[_paramPage] = page!.toString();
-        endpointName = endpoint.name.replaceFirst(
-          '<tvId>',
-          tvId!,
-        );
-        break;
-    }
-    return Uri.https(
-      baseUrl,
-      endpointName,
-      params,
-    );
   }
 }
