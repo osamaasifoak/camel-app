d77a5128803fcbd29d8ada980ade1b163fe56176
Iandi Santulus <iandisantulusn@gmail.com>
2021-09-08 02:08:30 +0700

    added new `MovieDetailScreen` and `TVShowDetailScreen`

---
+++ lib/views/details/tvshow_detail_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+import '/core/constants/singletons_names.dart';
+import '/core/repositories/base_eshows_repo.dart';
+import '/core/repositories/base_fav_eshows_repo.dart';
+import '/views/_screen_templates/eshow_detail/screen/_eshow_detail_screen.dart';
+
+class TVShowDetailScreen extends StatelessWidget {
+  const TVShowDetailScreen({
+    Key? key,
+    required this.movieId,
+  }) : super(key: key);
+
+  final int movieId;
+
+  @override
+  Widget build(BuildContext context) {
+    return EShowDetailScreen(
+      id: movieId,
+      eShowRepo: GetIt.I<BaseEShowsRepository>(instanceName: SIName.repo.tvShows),
+      favEShowRepo: GetIt.I<BaseFavEShowsRepository>(instanceName: SIName.repo.favTVShows),
+    );
+  }
+}
