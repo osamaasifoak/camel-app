93cf9f2c26ac35c74be0435d429070bae2695617
Iandi Santulus <iandisantulusn@gmail.com>
2021-09-08 01:39:36 +0700

    modified ScreenSizer
    - added currentHeight
    - added currentWidth
    - added currentAspectRatio

--- lib/core/helpers/screen_sizer.dart
+++ lib/core/helpers/screen_sizer.dart
@@ -6,4 +6,16 @@ class ScreenSizer {
 
   /// current half horizontal padding 
   double currentXPadding = 0;
+
+  double currentHeight = 0;
+  
+  double _currentWidth = 0;
+  double get currentWidth => _currentWidth;
+  set currentWidth(double newWidth) {
+    if(newWidth == _currentWidth) return;
+
+    _currentWidth = newWidth - currentXPadding * 2;
+  }
+
+  double get currentAspectRatio => _currentWidth / currentHeight;
 }
