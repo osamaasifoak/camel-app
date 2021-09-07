9526976e015f8ca115129df4c4cd76767f4ea4be
Iandi Santulus <iandisantulusn@gmail.com>
2021-09-08 01:38:19 +0700

    added `cancelAllRequestsMadeBy` to `BaseEShowsRepository`
    this is to enable cancelling all requests made by an instance

--- lib/core/repositories/base_eshows_repo.dart
+++ lib/core/repositories/base_eshows_repo.dart
@@ -4,10 +4,12 @@ import '/core/models/entertainment_show/entertainment_show_details.dart';
 import '/core/models/entertainment_show/entertainment_show_reviews.dart';
 
 abstract class BaseEShowsRepository {
-  Future<List<EShow>> fetch({required String category, int page = 1});
-  Future<List<EShow>> fetchByIds({required List<int> ids});
-  Future<EShowDetails> getDetails({required int id});
-  Future<List<EShowReview>> getReviews({required int id, int page = 1});
+  Future<List<EShow>> fetch({required String category, int page = 1, Object? requester});
+  Future<List<EShow>> fetchByIds({required List<int> ids, Object? requester});
+  Future<EShowDetails> getDetails({required int id, Object? requester});
+  Future<List<EShowReview>> getReviews({required int id, int page = 1, Object? requester});
   Future<List<EShow>> search({required String keyword, int page = 1});
+  /// this does not include requests made with [search]
+  void cancelAllRequestsMadeBy(Object requester);
   void cancelLastSearch();
 }
