5016b2bd8db4d7ad363dc815963fbc7f97a7ee27
Iandi Santulus <iandisantulusn@gmail.com>
2021-09-09 14:52:48 +0700

    modified `ScreenSizer`
    it now extends `ChangeNotifier` so that the widgets listening to it can respond to screen size changes

--- lib/core/helpers/screen_sizer.dart
+++ lib/core/helpers/screen_sizer.dart
@@ -1,21 +1,55 @@
+import 'package:flutter/foundation.dart';
 
-class ScreenSizer {
+class ScreenSizer extends ChangeNotifier {
   factory ScreenSizer() => _instance;
   ScreenSizer._internal();
   static final ScreenSizer _instance = ScreenSizer._internal();
 
-  /// current half horizontal padding 
-  double currentXPadding = 0;
+  /// current half horizontal padding
+  double _currentXPadding = 0;
+  double get currentXPadding => _currentXPadding;
+  set currentXPadding(final double newXPadding) {
+    if (newXPadding == _currentXPadding) {
+      return;
+    }
 
-  double currentHeight = 0;
-  
-  double _currentWidth = 0;
-  double get currentWidth => _currentWidth;
-  set currentWidth(double newWidth) {
-    if(newWidth - currentXPadding * 2 == _currentWidth) return;
-
-    _currentWidth = newWidth - currentXPadding * 2;
+    _currentXPadding = newXPadding;
+    notifyListeners();
   }
 
-  double get currentAspectRatio => _currentWidth / currentHeight;
+  double _currentHeight = 0;
+  double get currentHeight => _currentHeight;
+  set currentHeight(final double newHeight) {
+    if (newHeight == _currentHeight) {
+      return;
+    }
+
+    _currentHeight = newHeight;
+    notifyListeners();
+  }
+
+  double _currentWidth = 0;
+  double get currentWidth => _currentWidth;
+  set currentWidth(final double newWidth) {
+    final double _newWidth = newWidth - _currentXPadding * 2;
+    if (_newWidth == _currentWidth) return;
+
+    _currentWidth = _newWidth;
+    notifyListeners();
+  }
+
+  double get currentAspectRatio => _currentWidth / _currentHeight;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ScreenSizer &&
+        other._currentXPadding == _currentXPadding &&
+        other._currentHeight == _currentHeight &&
+        other._currentWidth == _currentWidth;
+  }
+
+  @override
+  int get hashCode => _currentXPadding.hashCode ^ _currentHeight.hashCode ^ _currentWidth.hashCode;
 }
