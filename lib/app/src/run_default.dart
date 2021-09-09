a337cb665a118829df397d5a9c44f92529ef937d
Iandi Santulus <iandisantulusn@gmail.com>
2021-09-10 02:54:46 +0700

    fix bug on `run_default`
    it should've been a top-level function

--- lib/app/src/run_default.dart
+++ lib/app/src/run_default.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert' show jsonDecode;
 
 import 'package:flutter/foundation.dart' show compute;
@@ -7,8 +6,8 @@ import 'package:flutter/services.dart' show SystemChrome, SystemUiOverlayStyle;
 import 'package:postor/postor.dart' as postor show defaultJsonDecoder;
 
 import '/core/constants/app_apis.dart';
-import '/error_msg_handlers.dart';
-import '/singletons.dart';
+import '/err_msg_handlers/err_msg_handlers.dart';
+import '/singletons/singletons.dart';
 import 'app_default.dart';
 
 Future<void> run() async {
@@ -29,16 +28,6 @@ Future<void> run() async {
   runApp(const App());
 }
 
-const Future<dynamic> Function(String source) _appIsolatedJsonDecoder = __appIsolatedJsonDecoder;
-
-Future<dynamic> __appIsolatedJsonDecoder(String source) {
-  return compute(
-    (String source) => jsonDecode(
-      source,
-      reviver: (_, __) {
-        // no-op
-      },
-    ),
-    source,
-  );
+Future<dynamic> _appIsolatedJsonDecoder(final String source) {
+  return compute(jsonDecode, source);
 }
